fb8c5f61c96f9325b87352148230b9a2633d4f3c
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2015-04-01 12:48:32 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Double-Dee-Menus
-Basic wrapper for adding and removing an open class to elements based upon a click
+
+A basic wrapper for adding and removing an open class to elements based upon a click
+
+## Usage
+
+Pass in your two elements, first the click object and second the menu object
+
+```html
+    <a href="" id="MenuButton">Book With Us</a>
+    
+    <ul id="menu-ul">
+        <li><a href="#">Item One</a></li>
+        <li><a href="#">Item Two</a></li>
+        <li><a href="#">Item Three</a></li>
+    </ul>
+```
+
+```javascript
+    var headerMenu = menu(document.getElementById('MenuButton'), document.getElementById('menu-ul'));
+```
+
+
+You can also programmatically toggle the menu using the public API
+
+```
+    var headerMenu = menu(document.getElementById('MenuButton'), document.getElementById('menu-ul'));
+   
+    // Add the open class
+    headerMenu.open();
+    
+    // Remove the open class
+    headerMenu.close();
+```
+
+## Contributing
+
+If you feel like contributing, add any issues you run across or even better, submit a PR :)
---
+++ src/double-dee-menus.js
@@ -0,0 +1,73 @@
+/**
+ * Easy wrapper for opening and closing menu items
+ *
+ * @param button
+ * @param menu
+ * @returns {{open: open, close: (close|Function|Window.close)}}
+ */
+function menu(button, menu) {
+
+    /**
+     * Check for button clicks and handle them
+     */
+    if (button.addEventListener) {
+        button.addEventListener('click', toggleMenu);
+    } else {
+        button.attachEvent('on' + 'click', function(){
+            toggleMenu.call();
+        });
+    }
+
+    /**
+     * Private function used to toggle the menu on click
+     *
+     * @param event
+     */
+    function toggleMenu(event) {
+        console.log(menu);
+        event.preventDefault();
+
+        if (menu.classList) {
+            menu.classList.toggle('open');
+        } else {
+            var classes = menu.className.split(' ');
+            var existingIndex = -1;
+            for (var i = classes.length; i--;) {
+                if (classes[i] === 'open')
+                    existingIndex = i;
+            }
+
+            if (existingIndex >= 0)
+                classes.splice(existingIndex, 1);
+            else
+                classes.push('open');
+
+            menu.className = classes.join(' ');
+        }
+    }
+
+    /**
+     * Manually close the menu
+     */
+    function open() {
+        if (menu.classList)
+            menu.classList.add('open');
+        else
+            menu.className += ' ' + 'open';
+    }
+
+    /**
+     * Manually open the menu
+     */
+    function close() {
+        if (menu.classList)
+            menu.classList.remove('open');
+        else
+            menu.className = menu.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+
+    return {
+        open: open,
+        close: close,
+    }
+}
